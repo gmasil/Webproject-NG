511748ee419cda9bb5cee5ca687271c6c90838ce
gmasil <54176035+gmasil@users.noreply.github.com>
2021-08-28 00:16:15 +0200

    add properties and unicode property loader

---
+++ webproject-backend/src/main/java/de/gmasil/webproject/config/UnicodePropertySourceLoader.java
@@ -0,0 +1,53 @@
+/*
+ * Webproject NG
+ * Copyright © 2021 Gmasil
+ *
+ * This file is part of Webproject NG.
+ *
+ * Webproject NG is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Webproject NG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Webproject NG. If not, see <https://www.gnu.org/licenses/>.
+ */
+package de.gmasil.webproject.config;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Properties;
+
+import org.springframework.boot.env.PropertySourceLoader;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UnicodePropertySourceLoader implements PropertySourceLoader {
+
+    @Override
+    public String[] getFileExtensions() {
+        return new String[] { "properties" };
+    }
+
+    @Override
+    public List<PropertySource<?>> load(String name, Resource resource) throws IOException {
+        Properties properties = new Properties();
+        properties.load(new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8));
+        if (!properties.isEmpty()) {
+            return Arrays.asList(new PropertiesPropertySource(name, properties));
+        }
+        return new LinkedList<>();
+    }
+}

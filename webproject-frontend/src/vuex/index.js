c38d5f0608703c760a0368b462643f1530a5f5ab
gmasil <54176035+gmasil@users.noreply.github.com>
2021-08-28 23:22:41 +0200

    add vuex minimal example

--- webproject-frontend/src/vuex/index.js
+++ webproject-frontend/src/vuex/index.js
@@ -17,19 +17,18 @@
  * You should have received a copy of the GNU General Public License
  * along with Webproject NG. If not, see <https://www.gnu.org/licenses/>.
  */
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
-import store from './vuex'
+import Vuex from 'vuex'
 
-Vue.config.productionTip = false;
+Vue.use(Vuex)
 
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>',
-  store
+export default new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
 })

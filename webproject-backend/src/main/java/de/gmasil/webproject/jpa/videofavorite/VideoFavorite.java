3290c18a9aabec5f2d6021d2bd3d6b32d11e9003
gmasil <54176035+gmasil@users.noreply.github.com>
2021-08-29 02:30:40 +0200

    patch videofavorite entity

---
+++ webproject-backend/src/main/java/de/gmasil/webproject/jpa/videofavorite/VideoFavorite.java
@@ -0,0 +1,71 @@
+/*
+ * Webproject NG
+ * Copyright © 2021 Gmasil
+ *
+ * This file is part of Webproject NG.
+ *
+ * Webproject NG is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Webproject NG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Webproject NG. If not, see <https://www.gnu.org/licenses/>.
+ */
+package de.gmasil.webproject.jpa.videofavorite;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import de.gmasil.webproject.jpa.Auditable;
+import de.gmasil.webproject.jpa.user.User;
+import de.gmasil.webproject.jpa.video.Video;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "VIDEO_FAVORITES")
+@Table(name = "VIDEO_FAVORITES")
+public class VideoFavorite extends Auditable {
+
+    @Id
+    @Setter(AccessLevel.NONE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "VIDEO_ID")
+    private Video video;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
--- webproject-backend/src/main/java/de/gmasil/webproject/jpa/videofavorite/VideoFavoriteRepository.java
+++ webproject-backend/src/main/java/de/gmasil/webproject/jpa/videofavorite/VideoFavoriteRepository.java
@@ -23,5 +23,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VideoFavoriteRepository extends JpaRepository<VideoFavoriteDAO, Long> {
+public interface VideoFavoriteRepository extends JpaRepository<VideoFavorite, Long> {
 }

1c1ddaf6e098dc0c516211390411fe8c2aa0d77d
gmasil <54176035+gmasil@users.noreply.github.com>
2021-08-29 23:10:25 +0200

    add license headers

--- webproject-backend/src/test/java/de/gmasil/webproject/jpa/artist/ArtistTest.java
+++ webproject-backend/src/test/java/de/gmasil/webproject/jpa/artist/ArtistTest.java
@@ -1,3 +1,22 @@
+/*
+ * Webproject NG
+ * Copyright © 2021 Gmasil
+ *
+ * This file is part of Webproject NG.
+ *
+ * Webproject NG is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Webproject NG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Webproject NG. If not, see <https://www.gnu.org/licenses/>.
+ */
 package de.gmasil.webproject.jpa.artist;
 
 import static org.hamcrest.CoreMatchers.equalTo;

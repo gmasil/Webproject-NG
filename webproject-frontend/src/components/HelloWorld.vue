db4ec7fa887d5586f7b5cceb0f7102d2f2dadc80
gmasil <54176035+gmasil@users.noreply.github.com>
2021-10-09 00:49:21 +0200

    fix sonar findings

--- webproject-frontend/src/components/HelloWorld.vue
+++ webproject-frontend/src/components/HelloWorld.vue
@@ -25,20 +25,32 @@
     <h2>Essential Links</h2>
     <ul>
       <li>
-        <a href="https://vuejs.org" target="_blank"> Core Docs </a>
+        <a href="https://vuejs.org" target="_blank" rel="noopener">
+          Core Docs
+        </a>
       </li>
       <li>
-        <a href="https://forum.vuejs.org" target="_blank"> Forum </a>
+        <a href="https://forum.vuejs.org" target="_blank" rel="noopener">
+          Forum
+        </a>
       </li>
       <li>
-        <a href="https://chat.vuejs.org" target="_blank"> Community Chat </a>
+        <a href="https://chat.vuejs.org" target="_blank" rel="noopener">
+          Community Chat
+        </a>
       </li>
       <li>
-        <a href="https://twitter.com/vuejs" target="_blank"> Twitter </a>
+        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">
+          Twitter
+        </a>
       </li>
       <br />
       <li>
-        <a href="http://vuejs-templates.github.io/webpack/" target="_blank">
+        <a
+          href="http://vuejs-templates.github.io/webpack/"
+          target="_blank"
+          rel="noopener"
+        >
           Docs for This Template
         </a>
       </li>
@@ -46,16 +58,26 @@
     <h2>Ecosystem</h2>
     <ul>
       <li>
-        <a href="http://router.vuejs.org/" target="_blank"> vue-router </a>
+        <a href="http://router.vuejs.org/" target="_blank" rel="noopener">
+          vue-router
+        </a>
       </li>
       <li>
-        <a href="http://vuex.vuejs.org/" target="_blank"> vuex </a>
+        <a href="http://vuex.vuejs.org/" target="_blank" rel="noopener">
+          vuex
+        </a>
       </li>
       <li>
-        <a href="http://vue-loader.vuejs.org/" target="_blank"> vue-loader </a>
+        <a href="http://vue-loader.vuejs.org/" target="_blank" rel="noopener">
+          vue-loader
+        </a>
       </li>
       <li>
-        <a href="https://github.com/vuejs/awesome-vue" target="_blank">
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+          rel="noopener"
+        >
           awesome-vue
         </a>
       </li>

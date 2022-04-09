b6b2ecce79fa2be93ec162ad6e3b4c7a7b41b979
gmasil <54176035+gmasil@users.noreply.github.com>
2022-04-09 14:57:50 +0200

    add license headers to vue3 files

--- webproject-frontend-vue3/frontend-vue3/postcss.config.js
+++ webproject-frontend-vue3/frontend-vue3/postcss.config.js
@@ -1,3 +1,20 @@
+/*
+ * Webproject NG
+ * Copyright © 2021 - 2022 Gmasil
+ *
+ * This file is part of Webproject NG.
+ *
+ * Webproject NG is licensed under the Creative Commons
+ * Attribution-NonCommercial-ShareAlike 4.0 International
+ * Public License ("Public License").
+ *
+ * Webproject NG is non-free software: you can redistribute
+ * it and/or modify it under the terms of the Public License.
+ *
+ * You should have received a copy of the Public License along
+ * with Webproject NG. If not, see
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode.txt
+ */
 module.exports = {
   plugins: {
     tailwindcss: {},

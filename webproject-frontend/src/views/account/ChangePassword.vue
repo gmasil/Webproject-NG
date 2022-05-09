293ffae02cb10c316079c5a2232f2206384fdae0
gmasil <54176035+gmasil@users.noreply.github.com>
2022-05-09 22:45:05 +0200

    update change password dialog

--- webproject-frontend/src/views/account/ChangePassword.vue
+++ webproject-frontend/src/views/account/ChangePassword.vue
@@ -18,39 +18,60 @@
 
 -->
 <template>
-  <div>
-    <label for="inputCurrentPassword">Current Password:</label>
-    <input
-      id="inputCurrentPassword"
-      v-model="currentPassword"
-      class="block"
-      type="password"
-    />
-    <label for="inputNewPassword">Current Password:</label>
-    <input
-      id="inputNewPassword"
-      v-model="newPassword"
-      class="block"
-      type="password"
-    />
-    <label for="inputRepeatPassword">Current Password:</label>
-    <input
-      id="inputRepeatPassword"
-      v-model="repeatPassword"
-      class="block"
-      type="password"
-    />
-    <button
-      class="border-2 border-theme-text bg-theme-background px-2 py-1"
-      @click="onChangeClick"
-    >
-      Change Password
-    </button>
+  <div class="grid justify-items-center -m-4">
+    <div class="text-left pt-20 text-lg w-full sm:w-sm p-4">
+      <h1 class="text-2xl mb-4">Change Password</h1>
+      <div class="grid grid-cols-2 gap-4">
+        <div class="col-span-2">
+          <label class="pl-2 block" for="inputCurrentPassword"
+            >Current Password:</label
+          >
+          <input
+            id="input-current-password"
+            v-model="data.currentPassword"
+            class="w-full p-2 rounded-lg"
+            type="password"
+            @keyup.enter="onChangeClick"
+          />
+        </div>
+        <div class="col-span-2">
+          <label class="pl-2 block" for="inputNewPassword">New Password:</label>
+          <input
+            id="input-new-password"
+            v-model="data.newPassword"
+            class="w-full p-2 rounded-lg"
+            type="password"
+            @keyup.enter="onChangeClick"
+          />
+        </div>
+        <div class="col-span-2">
+          <label class="pl-2 block" for="inputRepeatPassword"
+            >New Password:</label
+          >
+          <input
+            id="input-repeat-password"
+            v-model="data.repeatPassword"
+            class="w-full p-2 rounded-lg"
+            type="password"
+            @keyup.enter="onChangeClick"
+          />
+        </div>
+        <div class="col-span-2 justify-self-end">
+          <button
+            id="submit-change-password"
+            class="px-4 py-1 rounded-lg cursor-pointer bg-theme-background-highlight"
+            @click="onChangeClick"
+          >
+            Change Password
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script setup lang="ts">
+import { reactive } from "vue";
 import { userService } from "@/service/user";
 import { ChangePasswordData } from "@/types";
 import { ToastInterface, useToast } from "vue-toastification";
@@ -58,37 +79,44 @@ import { AxiosError } from "axios";
 
 const toast: ToastInterface = useToast();
 
-export default defineComponent({
-  name: "ChangePassword",
-  data() {
-    return {
-      currentPassword: "",
-      newPassword: "",
-      repeatPassword: "",
-    };
-  },
-  methods: {
-    onChangeClick(): void {
-      if (this.newPassword != this.repeatPassword) {
-        toast.error("Passwords mismatch");
-        return;
-      }
-      if (this.newPassword.length < 8) {
-        toast.error("Passwords must have at least 8 characters");
-        return;
-      }
-      userService
-        .changePassword({
-          currentPassword: this.currentPassword,
-          newPassword: this.newPassword,
-        } as ChangePasswordData)
-        .then(() => {
-          toast.success("Password changed successsfully");
-        })
-        .catch((error: AxiosError) => {
-          toast.error("Error changing password: " + error.message);
-        });
-    },
-  },
+interface BaseComponentData {
+  currentPassword: string;
+  newPassword: string;
+  repeatPassword: string;
+}
+
+const data: BaseComponentData = reactive({
+  currentPassword: "",
+  newPassword: "",
+  repeatPassword: "",
+});
+
+function onChangeClick(): void {
+  if (data.newPassword != data.repeatPassword) {
+    toast.error("Passwords mismatch");
+    return;
+  }
+  if (data.newPassword.length < 8) {
+    toast.error("Passwords must have at least 8 characters");
+    return;
+  }
+  userService
+    .changePassword({
+      currentPassword: data.currentPassword,
+      newPassword: data.newPassword,
+    } as ChangePasswordData)
+    .then(() => {
+      toast.success("Password changed successsfully");
+      data.currentPassword = "";
+      data.newPassword = "";
+      data.repeatPassword = "";
+    })
+    .catch((error: AxiosError) => {
+      toast.error("Error changing password: " + error.message);
+    });
+}
+
+defineExpose({
+  onChangeClick,
 });
 </script>
